d12a02e49865e792186878384bd3241f018ad982
Ugo Popée <ugo.popee@me.com>
2019-10-20 00:20:10 +0200

    Added empty install_requires field

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ def readme():
 
 
 setuptools.setup(
+    author='Ugo Popée',
+    author_email='ugo.popee@me.com',
     name='package-name',
     version='0.1',
+    url='http://github.com/bil0u/package-name',
     description='package-name description',
     long_description=readme(),
+    keywords='package-name',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
         'Topic :: Text Processing :: Linguistic',
     ],
-    keywords='package-name',
-    url='http://github.com/bil0u/package-name',
-    author='Ugo Popée',
-    author_email='ugo.popee@me.com',
     license='MIT',
     packages=[
         'package-name'
@@ -32,4 +32,5 @@ setuptools.setup(
             'package-name=package-name.app:main'
         ],
     },
+    install_requires=[]
 )
